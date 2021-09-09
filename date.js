98288b9e111d618d8867c1dedff40fc13458c6b6
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-09-09 12:24:11 +0100

    Initial Commit

---
+++ date.js
@@ -0,0 +1,17 @@
+module.exports.getDate = function () {
+let today = new Date();
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    return day = today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function () {
+    let today = new Date();
+        let options = {
+            weekday: 'long'
+        };
+        return day = today.toLocaleDateString("en-US", options);
+    }
